c270ca1312627ffda616d9b15edc6ed6e61c3089
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2025-01-21 04:54:15 +0100

    feat: enhance CastSyncPlugin to manage player state and playlist updates via socket

--- src/lib/VideoPlayer/plugins/castSyncPlugin.ts
+++ src/lib/VideoPlayer/plugins/castSyncPlugin.ts
@@ -1,5 +1,5 @@
 import Plugin from '@nomercy-entertainment/nomercy-video-player/src/plugin';
-import type {NMPlayer, TimeData} from "@nomercy-entertainment/nomercy-video-player/src/types";
+import type {NMPlayer, PlaylistItem, TimeData} from "@nomercy-entertainment/nomercy-video-player/src/types";
 import {useCastSocket} from "@/store/socket";
 import {initializeCastSocket} from "@/lib/socketClient/initializeSocket";
 
@@ -29,6 +29,9 @@ export class CastSyncPlugin extends Plugin {
 		this.player.on('volume', this.volume.bind(this));
 		this.player.on('muted', this.muted.bind(this));
 		this.player.on('playlist', this.playlist.bind(this));
+
+		const socket = useCastSocket();
+		socket?.on('GetPlayerState', this.getPlayerState.bind(this));
 	}
 
 	dispose() {
@@ -40,11 +43,17 @@ export class CastSyncPlugin extends Plugin {
 		// @ts-ignore
 		this.player.off('seek', this.seek.bind(this));
 		this.player.off('ended', this.ended.bind(this));
+		// @ts-ignore
 		this.player.off('item', this.item.bind(this));
 		// @ts-ignore
 		this.player.off('volume', this.volume.bind(this));
+		// @ts-ignore
 		this.player.off('muted', this.muted.bind(this));
+		// @ts-ignore
 		this.player.off('playlist', this.playlist.bind(this));
+
+		const socket = useCastSocket();
+		socket?.off('GetPlayerState', this.getPlayerState.bind(this));
 	}
 
 
@@ -83,21 +92,35 @@ export class CastSyncPlugin extends Plugin {
 		socket?.invoke?.('Volume', 'receiver', value);
 	}
 
-	muted() {
+	muted(value: boolean) {
 		const socket = useCastSocket();
-		socket?.invoke?.('Muted', 'receiver', this.player.isMuted());
+		socket?.invoke?.('Muted', 'receiver', value);
 	}
 
-	item() {
-		const current = this.player.playlistItem();
+	item(value: PlaylistItem) {
 		const socket = useCastSocket();
-		socket?.invoke?.('Item', 'receiver', current);
+		socket?.invoke?.('Item', 'receiver', value);
 	}
 
-	playlist() {
-		const playlist = this.player.getPlaylist();
+	playlist(value: PlaylistItem[]) {
 		const socket = useCastSocket();
-		socket?.invoke?.('Playlist', 'receiver', playlist);
+		socket?.invoke?.('Playlist', 'receiver', value);
+	}
+
+	getPlayerState() {
+		const socket = useCastSocket();
+		socket?.invoke?.('PlayerState', 'receiver', {
+			time: this.player.getTimeData(),
+			volume: this.player.getVolume(),
+			muted: this.player.getMute(),
+			playlist: this.player.getPlaylist(),
+			item: this.player.getPlaylistItem(),
+			isPlaying: this.player.isPlaying,
+			subtitles: this.player.getCaptionsList(),
+			currentSubtitleTrack: this.player.getCurrentCaptions(),
+			audioTracks: this.player.getAudioTracks(),
+			currentAudioTrack: this.player.getCurrentAudioTrack(),
+		});
 	}
 
 }
