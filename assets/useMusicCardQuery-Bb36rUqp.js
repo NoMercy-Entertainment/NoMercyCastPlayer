d21672a96288ce817a338c9de569c1485f6c1789
StoneyEagle <StoneyEagle@users.noreply.github.com>
2026-04-29 01:13:47 +0000

    deploy: edec6b54339cecb78f6a91c02ea6095a7ce0845a

--- assets/useMusicCardQuery-Bb36rUqp.js
+++
@@ -1,2 +0,0 @@
-import{a as e}from"./_plugin-vue_export-helper-8ASmFJWo.js";import{n as t,r as n,t as r}from"./client-DcIx5vz1.js";import{n as i}from"./index-BvmT7Lca.js";import{n as a}from"./EmptyState-BvOsntOb.js";var o=/^\//;async function s(e){let t=await r({path:`/api/v1/music/${e.replace(o,``)}`,method:`GET`});return Array.isArray(t)?t:t.components??[]}function c(r){let o=e(()=>[...t.musicCards(r.value)]),c=n({queryKey:o,queryFn:()=>s(r.value),...i.standard});return a(o.value,c.data,c.isLoading,c.isFetching),c}export{c as t};
-//# sourceMappingURL=useMusicCardQuery-Bb36rUqp.js.map
---
+++ assets/useMusicCardQuery-sJusXAUU.js
@@ -0,0 +1,2 @@
+import{a as e}from"./_plugin-vue_export-helper-8ASmFJWo.js";import{n as t,r as n,t as r}from"./client-0bBFcgwK.js";import{n as i}from"./index-ZnpB004R.js";import{n as a}from"./EmptyState-BBtiJodw.js";var o=/^\//;async function s(e){let t=await r({path:`/api/v1/music/${e.replace(o,``)}`,method:`GET`});return Array.isArray(t)?t:t.components??[]}function c(r){let o=e(()=>[...t.musicCards(r.value)]),c=n({queryKey:o,queryFn:()=>s(r.value),...i.standard});return a(o.value,c.data,c.isLoading,c.isFetching),c}export{c as t};
+//# sourceMappingURL=useMusicCardQuery-sJusXAUU.js.map
