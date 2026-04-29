13affa397810a04df9399030e4bcfd552bee6c5c
StoneyEagle <StoneyEagle@users.noreply.github.com>
2026-04-29 01:11:34 +0000

    deploy: 60d60de5b6c78280ee0d68bd4a3e6d22ac8820a5

--- assets/useMusicCardQuery-DLVbT5Ee.js
+++
@@ -1,2 +0,0 @@
-import{a as e}from"./_plugin-vue_export-helper-8ASmFJWo.js";import{n as t,r as n,t as r}from"./client-CP602q2M.js";import{n as i}from"./index-CgOfhutp.js";import{n as a}from"./EmptyState-CMdGphX1.js";var o=/^\//;async function s(e){let t=await r({path:`/api/v1/music/${e.replace(o,``)}`,method:`GET`});return Array.isArray(t)?t:t.components??[]}function c(r){let o=e(()=>[...t.musicCards(r.value)]),c=n({queryKey:o,queryFn:()=>s(r.value),...i.standard});return a(o.value,c.data,c.isLoading,c.isFetching),c}export{c as t};
-//# sourceMappingURL=useMusicCardQuery-DLVbT5Ee.js.map
