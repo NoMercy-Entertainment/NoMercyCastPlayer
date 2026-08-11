9799bed8722686cc768e4c79baa4c529f97be7b6
StoneyEagle <StoneyEagle@users.noreply.github.com>
2026-08-11 15:06:56 +0000

    deploy: 8ee5f5c5780cd272a0271e1fe1e67600da9f35c0

---
+++ assets/useMeQuery-Drd9YLQe.js
@@ -0,0 +1,2 @@
+import{s as e}from"./_plugin-vue_export-helper-C1IiIKbV.js";import{t}from"./useQuery-Cj5sQN1N.js";import{t as n}from"./authStore-1VM7avA2.js";import{r}from"./index-76nxNG3f.js";function i(){return t({queryKey:[`me`],queryFn:()=>r(n.accessToken.value??``),enabled:e(()=>!!n.accessToken.value),staleTime:5*6e4,refetchOnWindowFocus:!1})}export{i as t};
+//# sourceMappingURL=useMeQuery-Drd9YLQe.js.map
